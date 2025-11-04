ff3e1eae483122316eb18e0d6510edde204e3a7f
Mukilanbalakrishnan <154340556+Mukilanbalakrishnan@users.noreply.github.com>
2025-11-04 19:12:53 +0530

    init

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 important for Netlify
+  base: './', // 👈 important for Netlify
 })
